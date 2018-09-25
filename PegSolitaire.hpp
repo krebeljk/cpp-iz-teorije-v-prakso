4a70aa72c4fabbfc903dfd3d5a9273f99a091677
Kristjan <krebeljk@gmail.com>
2018-09-25 19:36:18 +0200

    init mat

--- PegSolitaire.hpp
+++ PegSolitaire.hpp
@@ -5,6 +5,7 @@ class EBoard
 private:
 	int maxX_;
 	int maxY_;
+	int thr_;
 
 	std::array<std::array<int, 9>, 9> state_;
 
@@ -12,12 +13,28 @@ public:
 	EBoard():
 		maxX_(9),
 		maxY_(9),
+		thr_(3),
 		state_()
 	{
 		for (int i = 0; i < maxX_; i++)//vrsta
 			for (int j = 0; j < maxY_; j++)//stolpec
 			{
-				state_[i][j] = 0;
+
+				// SL
+				if (i < thr_ && j < thr_)
+					state_[i][j] = -1;
+
+				// SD
+				if (i >= 2*thr_ && j < thr_)
+					state_[i][j] = -1;
+				
+				// ZL
+				if (i < thr_ && j >= 2*thr_)
+					state_[i][j] = -1;
+
+				if (i >= 2*thr_ && j >= 2*thr_)
+					state_[i][j] = -1;
+
 			}
 	
 	
