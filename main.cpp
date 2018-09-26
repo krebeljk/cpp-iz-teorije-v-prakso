611c02a9ca6c3993fbd4ebf20d0f46ae96615775
Kristjan <krebeljk@gmail.com>
2018-09-26 23:44:27 +0200

    first algo

--- main.cpp
+++ main.cpp
@@ -7,6 +7,37 @@ int main(){
 	std::vector<EBoard> seq{EBoard()};
 
 	seq.back().print();
+
+	// state index
+	int ind = 0;
+
+	// solution control
+	bool solve = true;
+
+	while (solve)
+	{
+		if(seq[ind].nextValidMoveIntent())
+		{ 
+			seq.push_back(seq[ind]);
+			ind++;
+			seq[ind].makeMove();
+		}
+		else if (seq.capacity()>0)
+		{
+			seq.pop_back();
+			ind--;
+		}
+		else
+		{
+			solve = false;
+		}
+		std::cout << "i " << ind;
+	}
+
+	for (auto &st : seq)
+	{
+		st.print();
+	}
 	
 
 
